d3ff8c35582cfbe29b4dbafac1686131f435040b
Nathalia <nathalia.cantarelli@edu.pucrs..br>
2022-08-03 18:29:10 -0300

    Requisitos 7 e 8 concluidos

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-function changeBackColor(posicaoAtual, classe) {
-  classe[posicaoAtual].className = "pixel selected";
+function changeBackColor(posicaoAtual, position, cor) {
+  position[posicaoAtual].id = cor;
 }
 
 const backColor = document.getElementsByClassName('pixel');
+const colorPaint = document.getElementsByClassName('color');
+
+let paint = 'black';
+
+for (let i = 0; i < colorPaint.length; i += 1) {  
+  colorPaint[i].addEventListener('click', function (event) {
+    let classAfter = document.getElementById(paint);
+    classAfter.className = 'color';
+    let colorAtual = event.target.id;
+    paint = colorAtual;
+    let classBefore = document.getElementById(paint);
+    classBefore.className = 'color selected';
+  });
+}
+
 for (let i = 0; i < backColor.length; i += 1) {
-  backColor[i].addEventListener('click', function() {
-    changeBackColor(i, backColor);
+  backColor[i].addEventListener('click', function () {
+    changeBackColor(i, backColor, paint);
   });
 }
