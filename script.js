7bccc37a7f2b8119d83b7ab104b06fcbb11ee70c
Nathalia <nathalia.cantarelli@edu.pucrs..br>
2022-08-04 01:15:14 -0300

    Requisito 12 concluido

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ let space = document.getElementById('pixel-board');
 const backColor = document.getElementsByClassName('pixel');
 const colorPaint = document.getElementsByClassName('color');
 
+let colorFirst = document.getElementById('color1');
+let colorSecond = document.getElementById('color2');
+let colorThird = document.getElementById('color3');
+
+colorFirst.style.backgroundColor = generateColor();
+colorSecond.style.backgroundColor = generateColor();
+colorThird.style.backgroundColor = generateColor();
+
 function definir () {
   inputNumber = document.getElementById('board-size').value;
 
@@ -35,35 +43,54 @@ function creatMatrix (inputNumber) {
 
 
 function changeBackColor(posicaoAtual, position, cor) {
-  position[posicaoAtual].id = cor;
+  position[posicaoAtual].style.backgroundColor = cor;
 }
 
 let paint = 'black';
+let newColor = 'black';
 
-for (let i = 0; i < colorPaint.length; i += 1) {  
+
+for (let i = 0; i < colorPaint.length; i += 1) { 
+  
   colorPaint[i].addEventListener('click', function (event) {
     let classAfter = document.getElementById(paint);
     classAfter.className = 'color';
     let colorAtual = event.target.id;
     paint = colorAtual;
     let classBefore = document.getElementById(paint);
+    classBefore.style.backgroundColor = paint;
     classBefore.className = 'color selected';
+    newColor = classBefore.style.backgroundColor;
+    console.log(newColor);
+    
   });
+  for (let i = 0; i < backColor.length; i += 1) { 
+      backColor[i].addEventListener('click', function () {
+        changeBackColor(i, backColor, newColor);
+      });
+    }
 }
 
-for (let i = 0; i < backColor.length; i += 1) { 
-  backColor[i].addEventListener('click', function () {
-    changeBackColor(i, backColor, paint);
-  });
-}
+
 
 const clickButton = document.getElementById('clear-board');
 clickButton.addEventListener('click',function() {
   let reset = document.getElementsByClassName('pixel');
   for (let j = 0; j < reset.length; j += 1) {
-    reset[j].id = '';
+    reset[j].style.backgroundColor = 'white';
   }
 })
 }
 
 creatMatrix(5);
+
+function generateColor() {
+  const options = '0123456789ABCDEF';
+  let colorDraw = '#';
+  
+  for (let b = 0; b < 6; b += 1) {
+    colorDraw += options[Math.floor(Math.random() * 16)];
+  }
+  return colorDraw;
+}
+
